454354cf48a754107e8c4238cd51556ceddaa6e5
Zar007110 <zvi007110@gmail.com>
2022-07-02 16:31:41 +0530

    2ndcommit

---
+++ lib/login.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String email = '', pass = '';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('login'),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(hintText: 'email'),
+              ),
+              TextField(
+                onChanged: (value) {
+                  pass = value;
+                },
+                obscureText: true,
+                decoration: InputDecoration(hintText: 'Password'),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: pass);
+                      Navigator.pushNamed(context, 'home');
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        print('No user found for that email.');
+                      } else if (e.code == 'wrong-password') {
+                        print('Wrong password provided for that user.');
+                      }
+                    }
+                  },
+                  child: Text('Submit')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
